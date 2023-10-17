c2e34bb24c6da735194cd8f9da1082fdedcc5fff
lucalin17081994 <agahamsc@live.com>
2023-10-17 15:41:27 +0200

    first

---
+++ src/Animal.java
@@ -0,0 +1,16 @@
+
+public class Animal {
+
+	//protected=binnen deze classe en alle sub classes
+	protected String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
